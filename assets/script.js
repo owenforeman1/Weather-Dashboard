695a7296751ea9e40fb5701fc48dfbe48df7730d
Owen Foreman <owen4man@gmail.com>
2022-03-25 22:03:43 -0400

    sidebar

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// bc4cde8c4ce64536e7377d99166a9442; weather-dashboard api key
+
+// var city;
+
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + bc4cde8c4ce64536e7377d99166a9442;
+
+// api.openweathermap.org/data/2.5/weather?q={Philidelphia}&appid = {bc4cde8c4ce64536e7377d99166a9442}
