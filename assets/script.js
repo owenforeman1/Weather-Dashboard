ea60d6cc4667b29fcc62fc60ca6b94847e48da58
Owen Foreman <owen4man@gmail.com>
2022-03-25 23:31:46 -0400

    js start

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,24 @@
-// bc4cde8c4ce64536e7377d99166a9442; weather-dashboard api key
-
-// var city;
-
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + bc4cde8c4ce64536e7377d99166a9442;
-
-// api.openweathermap.org/data/2.5/weather?q={Philidelphia}&appid = {bc4cde8c4ce64536e7377d99166a9442}
 
 
+var city = "Paris";
+var APIKey = "bc4cde8c4ce64536e7377d99166a9442";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL) 
+.then(function (response){
+  return response.json();
+})
+.then(function (data){
 
 
 
+  
+  console.log(data);
+})
+
+
+// lat lon for five day
+// 
 // function to search when clicked
 // function to have recent searches/local storage
 // function to display info on cards
