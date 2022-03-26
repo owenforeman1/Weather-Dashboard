108886e705e2136fc172c42d108d697b8a07ec9b
Owen Foreman <owen4man@gmail.com>
2022-03-25 23:01:12 -0400

    margins and hr

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,12 @@
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + bc4cde8c4ce64536e7377d99166a9442;
 
 // api.openweathermap.org/data/2.5/weather?q={Philidelphia}&appid = {bc4cde8c4ce64536e7377d99166a9442}
+
+
+
+
+
+// function to search when clicked
+// function to have recent searches/local storage
+// function to display info on cards
+// function for api call
